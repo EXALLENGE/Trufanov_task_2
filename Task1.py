199a699417ca5e265934552745797297ecfb01f0
NikitaTrufanov <trufanovnikita0@gmail.com>
2019-02-24 22:16:01 +0300

    fix in a new branch

--- Task1.py
+++ Task1.py
@@ -1,5 +1,5 @@
 import random
-words = ['imagination', 'pleasure', 'apple', 'homework', 'armageddon']
+words = ['imagination', 'pleasure', 'apple', 'homework']
 word = random.choice(words)
 lst_word = list(word)
 lst_guessed = ['*']*len(word)
